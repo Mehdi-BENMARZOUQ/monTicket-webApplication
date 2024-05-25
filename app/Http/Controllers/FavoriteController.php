7f5dffe7776e6c95dc0169ec9a01ab5cb9ffc0ac
Im-MB <bmarzouq03@gmail.com>
2024-05-25 02:57:00 +0100

    Mehdi003

---
+++ app/Http/Controllers/FavoriteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class FavoriteController extends Controller
+{
+    public function index()
+    {
+        $user = Auth::user();
+        $favoriteEvents = $user->favorites()->with('event')->get();
+        return view('favorites.index', compact('favoriteEvents'));
+    }
+
+
+    public function store(Request $request, $eventId)
+    {
+        $user = Auth::user();
+        $event = Event::findOrFail($eventId);
+
+        if ($user->favorites()->where('event_id', $eventId)->exists()) {
+            return back()->with('error', 'Event already in favorites.');
+        }
+
+        $user->favorites()->attach($eventId);
+
+        return back()->with('success', 'Event added to favorites.');
+    }
+
+    public function destroy(Request $request, $eventId)
+    {
+        $user = Auth::user();
+        $user->favorites()->detach($eventId);
+
+        return back()->with('success', 'Event removed from favorites.');
+    }
+}
